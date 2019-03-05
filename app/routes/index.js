6dc12d8ec2df6492f65b593b7643a819b78ba014
Richard <richard@tlgd.co.uk>
2019-03-05 11:09:03 +0000

    Back to tutorial code

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,7 @@
 import Route from '@ember/routing/route';
-import {Promise} from 'rsvp';
 
 export default Route.extend({
     beforeModel() {
-        return new Promise((res) => {
-            setTimeout(() => {
-                console.log(1)
-                res()
-            }, 2000)
-        })
-        console.log(1);
-        // this.replaceWith('rentals');
-    },
-    model() {
-       return {
-           name: 'Gabor'
-       }
-    }, 
-    afterModel(model) {
-        console.log(model)
-        console.log(3);
+        this.replaceWith('rentals');
     }
 });
