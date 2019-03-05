0f594f02cde8c3906b542aa29c4b0b84e41ca9f9
Richard <richard@tlgd.co.uk>
2019-03-05 11:08:07 +0000

    Gabor hooks example

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,24 @@
 import Route from '@ember/routing/route';
+import {Promise} from 'rsvp';
 
 export default Route.extend({
     beforeModel() {
-        this.replaceWith('rentals');
+        return new Promise((res) => {
+            setTimeout(() => {
+                console.log(1)
+                res()
+            }, 2000)
+        })
+        console.log(1);
+        // this.replaceWith('rentals');
+    },
+    model() {
+       return {
+           name: 'Gabor'
+       }
+    }, 
+    afterModel(model) {
+        console.log(model)
+        console.log(3);
     }
 });
