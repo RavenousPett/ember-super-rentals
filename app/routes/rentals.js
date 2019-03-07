594dd9a9b00f7b172e9736d1dad04d840d84aab4
Richard <richard@tlgd.co.uk>
2019-03-07 16:08:05 +0000

    Move the action from the parent controller the the child index controller

--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -1,19 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    actions: {
-        filterByCity(param) {
-        if (param !== '') {
-            return this.store
-            .query('rental', { city: param }).then((results) => {
-                return { query: param, results: results };
-            });
-        } else {
-            return this.store
-            .findAll('rental').then((results) => {
-                return { query: param, results: results };
-            });
-        }
-        }
-    }
 });
--- app/routes/rentals/index.js
+++ app/routes/rentals/index.js
@@ -3,5 +3,20 @@ import Route from '@ember/routing/route';
 export default Route.extend({
     model() {
         return this.store.findAll('rental');
+    },
+    actions: {
+        filterByCity(param) {
+        if (param !== '') {
+            return this.store
+            .query('rental', { city: param }).then((results) => {
+                return { query: param, results: results };
+            });
+        } else {
+            return this.store
+            .findAll('rental').then((results) => {
+                return { query: param, results: results };
+            });
+        }
+        }
     }
 });
