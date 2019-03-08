83aed1b88b2958a6e9dd0328af5f824d706323ce
Richard <richard@tlgd.co.uk>
2019-03-08 11:34:08 +0000

    Tidy up

--- app/routes/rentals/index.js
+++ app/routes/rentals/index.js
@@ -3,22 +3,16 @@ import EmberObject from '@ember/object'
 
 export default Route.extend({
     model() {
-
         return EmberObject.create({
             results: this.store.findAll('rental')
         });
-
-    },
-    afterModel({results}) {
-        console.log(results.get('length'));
     },
     actions: {
         filterByCity(param) {
             const model = this.modelFor(this.routeName); // 'rentals.index'
-            console.log('model', model);
+
             return this.store
             .query('rental', { city: param }).then((results) => {
-                console.log('query results', results);
                 model.set('results', results);
             });
         }
